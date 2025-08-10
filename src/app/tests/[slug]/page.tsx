eef9b03f4ff6390451683d7f08025f013df71600
pushpander1010 <2023mt93337@wilp.bits-pilani.ac.in>
2025-08-10 17:41:25 +0530

    fixed

--- src/app/tests/[slug]/page.tsx
+++ src/app/tests/[slug]/page.tsx
@@ -20,6 +20,7 @@ export default async function TestDetailPage({
   params,
 }: {
   params: { slug: string };
+  searchParams?: { [key: string]: string | string[] | undefined };
 }) {
   const { slug } = params;
 
